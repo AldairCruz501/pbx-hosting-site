bf02dc8be73afe69593ec1e7cd9670199b32fc12
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-04-29 13:57:57 -0600

    Actualizacion de valores

--- src/components/ValuesComponent.vue
+++ src/components/ValuesComponent.vue
@@ -9,15 +9,15 @@ const formatNumber = (num: number) => num.toString().padStart(2, '0')
     <div class="container text-center">
       <h2 class="fw-bold mb-5">Nuestros Valores</h2>
       <div class="row g-4">
-        <div class="col-12 col-md-6 col-xl-4" v-for="(value, index) in values" :key="index">
-          <div class="d-flex align-items-start flex-column text-start">
-            <div class="rounded-circle bg-white text-value fw-bold d-flex justify-content-center align-items-center mb-3" style="width: 60px; height: 60px;">
-              {{ formatNumber(index + 1) }}
-            </div>
-            <h5 class="fw-bold">{{ value.titulo }}</h5>
-            <p class="text-white-50">{{ value.descripcion }}</p>
-          </div>
-        </div>
+				<div class="col-12 col-md-6 col-xl-4" v-for="(value, index) in values" :key="index">
+					<div class="value-card p-4 rounded-4 h-100 d-flex flex-column align-items-start text-start transition">
+						<div class="rounded-circle bg-white text-value fw-bold d-flex justify-content-center align-items-center mb-3" style="width: 60px; height: 60px;">
+							{{ formatNumber(index + 1) }}
+						</div>
+						<h5 class="fw-bold">{{ value.titulo }}</h5>
+						<p class="text-white-50">{{ value.descripcion }}</p>
+					</div>
+				</div>
       </div>
     </div>
   </section>
@@ -30,4 +30,15 @@ const formatNumber = (num: number) => num.toString().padStart(2, '0')
 	.text-value {
 		color: var(--secondary_color);
 	}
+	.value-card {
+  background-color: transparent;
+  transition: transform 0.3s ease, background-color 0.3s ease;
+}
+
+.value-card:hover {
+  background-color: rgba(255, 255, 255, 0.1); /* Cambia este valor si quieres otro color */
+  transform: scale(1.05);
+  cursor: pointer;
+}
+
 </style>
