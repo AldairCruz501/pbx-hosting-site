7404df2700318d8f733b10e7d0344b9822c22396
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-05-05 16:07:15 -0600

    Creacion de componente de carga (spinner)

---
+++ src/components/SpinnerComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="loading-overlay">
+        <fulfilling-square-spinner
+            :animation-duration="4000"
+            :size="50"
+            color="#be222a"
+        />
+    </div>
+</template>
+
+<script setup>
+import { FulfillingSquareSpinner } from 'epic-spinners'
+</script>
+
+<style scoped>
+.loading-overlay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+}
+</style>
