271a0b0c493e6e97ea9fcc9154815ca46cb23328
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-05-05 16:06:28 -0600

    Datos para la pagina de Telefonia IP

---
+++ src/data/iptelephony.ts
@@ -0,0 +1,98 @@
+import type { IPTelephonyData } from "../interfaces/IPTelephony";
+
+export const ipTelephonyData: IPTelephonyData = {
+    telfunctions: [
+        {
+            titulo: "Ahorro en llamadas",
+            img: "/img/ahorro-llamadas.svg"
+        },
+        {
+            titulo: "Sin Límites Geográficos",
+            img: "/img/llamadas-internacionales.svg"
+        },
+        {
+            titulo: "Soporte 24/7",
+            img: "/img/soporte-24horas.svg"
+        },
+        {
+            titulo: "Portabilidad",
+            img: "/img/portabilidad-numeros-virtuales.svg"
+        },
+        {
+            titulo: "Calidad de llamadas HD",
+            img: "/img/llamadas-altacalidad.svg"
+        },
+        {
+            titulo: "Línea 800 TollFree",
+            img: "/img/linea-800.svg"
+        },
+    ],
+    benfits: [
+        {
+            titulo: "Conexión estable",
+            descripcion: "Comunicación clara y sin interrupciones."
+        },
+        {
+            titulo: "Múltiples líneas",
+            descripcion: "Excelente gestión de llamadas simúltaneas."
+        },
+        {
+            titulo: "Funciones Avanzadas",
+            descripcion: "Mejoran la eficiencia y productividad."
+        },
+        {
+            titulo: "Escalabilidad",
+            descripcion: "Adaptable al crecimiento de la empresa para cubrir las necesidades."
+        },
+    ],
+    solutions:[
+        {
+            titulo: "Callcenter",
+            img: "bi bi-headset"
+        },
+        {
+            titulo: "Salud",
+            img: "bi bi-heart-pulse"
+        },
+        {
+            titulo: "Hoteles",
+            img: "bi bi-building"
+        },
+        {
+            titulo: "eCommerce",
+            img: "bi bi-cart"
+        },
+        {
+            titulo: "Escuelas",
+            img: "bi bi-backpack2"
+        },
+        {
+            titulo: "Bienes Raíces",
+            img: "bi bi-buildings"
+        },
+        {
+            titulo: "Transporte",
+            img: "bi bi-truck"
+        },
+        {
+            titulo: "Seguros",
+            img: "bi bi-postcard-heart"
+        },
+        {
+            titulo: "Restaurantes",
+            img: "bi bi-cup"
+        },
+        {
+            titulo: "Gobierno",
+            img: "bi bi-bank2"
+        },
+        {
+            titulo: "Finanzas",
+            img: "bi bi-cash-coin"
+        },
+        {
+            titulo: "ContactCenter",
+            img: "bi bi-headphones"
+        },
+    ],
+}
